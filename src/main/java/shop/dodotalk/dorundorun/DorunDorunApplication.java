01d9c753ca0677e9b9640fcf218ba14870136d99
kwon woo <dirn0568@naver.com>
2023-02-08 15:50:41 +0900

    first commit

---
+++ src/main/java/shop/dodotalk/dorundorun/DorunDorunApplication.java
@@ -0,0 +1,13 @@
+package shop.dodotalk.dorundorun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DorunDorunApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DorunDorunApplication.class, args);
+    }
+
+}
---
+++ src/test/java/shop/dodotalk/dorundorun/DorunDorunApplicationTests.java
@@ -0,0 +1,13 @@
+package shop.dodotalk.dorundorun;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DorunDorunApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

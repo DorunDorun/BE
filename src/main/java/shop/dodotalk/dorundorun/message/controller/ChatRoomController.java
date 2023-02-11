e5f0f96a8a8f1c4903fb3c52decfcbc18c0cad6f
kwon woo <dirn0568@naver.com>
2023-02-11 21:20:21 +0900

    타임리프 확인 컨트롤러 제거

--- src/main/java/shop/dodotalk/dorundorun/message/controller/ChatRoomController.java
+++ src/main/java/shop/dodotalk/dorundorun/message/controller/ChatRoomController.java
@@ -6,22 +6,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
-@Controller // @ResponseBody 필요할때 쓰기!
+@Controller
 @RequiredArgsConstructor
 public class ChatRoomController {
-    // socket test
-    @RequestMapping("/chat/room/{roomId}")
-    public ModelAndView chat() {
-        ModelAndView mv = new ModelAndView();
-        mv.setViewName("chat");
-        return mv;
-    }
-
-    // socket test
-    @RequestMapping("/chat/rooms")
-    public ModelAndView chatRooms() {
-        ModelAndView mv = new ModelAndView();
-        mv.setViewName("chatrooms");
-        return mv;
-    }
 }

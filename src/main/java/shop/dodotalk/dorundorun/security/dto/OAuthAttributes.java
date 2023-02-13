540eda1a9a0528afb798eaf08c665047e83e2813
hyunjunhwang1994 <hyunjunhwang1994@gmail.com>
2023-02-13 14:50:04 +0900

    OAuth / kakao, google, naver 추가

---
+++ src/main/java/shop/dodotalk/dorundorun/security/dto/OAuthAttributes.java
@@ -0,0 +1,84 @@
+package shop.dodotalk.dorundorun.security.dto;
+
+import shop.dodotalk.dorundorun.security.dto.UserProfile;
+
+import java.util.Arrays;
+import java.util.Map;
+import java.util.function.Function;
+
+public enum OAuthAttributes {
+    GOOGLE("google", (attributes) -> {
+        UserProfile userProfile = new UserProfile();
+
+
+        userProfile.setName((String) attributes.get("name"));
+        userProfile.setEmail((String) attributes.get("email"));
+        userProfile.setProfile((String) attributes.get("picture"));
+
+
+        return userProfile;
+    }),
+
+    NAVER("naver", (attributes) -> {
+        // 응답 형식
+        // id, nickname, profile_image, age, gender, email, mobile, mobile_e164, name, birthday, birthyear
+
+        // response에 네이버에서 사용자 정보에 대한 응답 데이터가 전달 됨.
+        Map<String, Object> response = (Map<String, Object>) attributes.get("response");
+
+        UserProfile userProfile = new UserProfile();
+
+        userProfile.setName((String) response.get("name"));
+        userProfile.setNickname((String) response.get("nickname"));
+
+        userProfile.setEmail(((String) response.get("email")));
+        userProfile.setProfile((String) response.get("profile_image"));
+
+        userProfile.setAge_range((String) response.get("age"));
+        userProfile.setGender((String) response.get("gender"));
+
+        userProfile.setMobile((String) response.get("mobile"));
+        userProfile.setMobile_e164((String) response.get("mobile_e164"));
+
+        userProfile.setBirthday((String) response.get("birthday"));
+        userProfile.setBirthyear((String) response.get("birthyear"));
+
+        return userProfile;
+    }),
+
+    KAKAO("kakao", (attributes) -> {
+        // kakao는 kakao_account에 유저정보가 있다. (email)
+        Map<String, Object> kakaoAccount = (Map<String, Object>) attributes.get("kakao_account");
+        // kakao_account안에 또 profile이라는 JSON객체가 있다. (nickname, profile_image)
+        Map<String, Object> kakaoProfile = (Map<String, Object>) kakaoAccount.get("profile");
+
+        UserProfile userProfile = new UserProfile();
+        userProfile.setName((String) kakaoProfile.get("nickname"));
+        userProfile.setEmail((String) kakaoAccount.get("email"));
+
+        userProfile.setProfile((String) kakaoProfile.get("profile_image_url"));
+        userProfile.setThumbnail((String) kakaoProfile.get("thumbnail_image_url"));
+
+        userProfile.setGender((String) kakaoAccount.get("gender"));
+        userProfile.setBirthday((String) kakaoAccount.get("birthday"));
+        userProfile.setAge_range((String) kakaoAccount.get("age_range"));
+        return userProfile;
+    });
+
+    private final String registrationId;
+    private final Function<Map<String, Object>, UserProfile> of;
+
+    OAuthAttributes(String registrationId, Function<Map<String, Object>, UserProfile> of) {
+        this.registrationId = registrationId;
+        this.of = of;
+    }
+
+    public static UserProfile extract(String registrationId, Map<String, Object> attributes) {
+        return Arrays.stream(values())
+                .filter(provider -> registrationId.equals(provider.registrationId))
+                .findFirst()
+                .orElseThrow(IllegalArgumentException::new)
+                .of.apply(attributes);
+    }
+
+}
---
+++ src/main/java/shop/dodotalk/dorundorun/security/handler/OAuth2SuccessHandler.java
@@ -0,0 +1,97 @@
+package shop.dodotalk.dorundorun.security.handler;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.token.TokenService;
+import org.springframework.security.oauth2.core.user.DefaultOAuth2User;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
+import org.springframework.stereotype.Component;
+import org.springframework.web.util.UriComponentsBuilder;
+import shop.dodotalk.dorundorun.security.jwt.JwtUtil;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.Map;
+
+import static shop.dodotalk.dorundorun.security.jwt.JwtAuthenticationFilter.AUTHORIZATION_HEADER;
+import static shop.dodotalk.dorundorun.security.jwt.JwtAuthenticationFilter.REFRESH_HEADER;
+
+@Slf4j
+@RequiredArgsConstructor
+@Component
+public class OAuth2SuccessHandler extends SimpleUrlAuthenticationSuccessHandler {
+
+    private final JwtUtil jwtUtil;
+
+    @Override
+    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication)
+            throws ServletException, IOException {
+        log.info("소셜 인증 성공--onAuthenticationSuccess() start");
+
+        OAuth2User oAuth2User = (OAuth2User) authentication.getPrincipal();
+        Map<String, Object> attributes = oAuth2User.getAttributes();
+
+
+        String targetUrl = UriComponentsBuilder.fromUriString("http://localhost:3000/home")
+                .build().encode(StandardCharsets.UTF_8)
+                .toUriString();
+
+
+        /* JWT Access Token 발급 헤더에 넣던지, 쿠키로 전달해주던지. */
+        log.info("access token 발급");
+//        response.addHeader(AUTHORIZATION_HEADER, "Bearer-" + jwtUtil.generateAccessToken(authentication));
+        Cookie accessTokenCookie = new Cookie(AUTHORIZATION_HEADER,"Bearer-" + jwtUtil.generateAccessToken(authentication));
+        accessTokenCookie.setPath("/");
+        accessTokenCookie.setHttpOnly(true);
+        response.addCookie(accessTokenCookie);
+
+        /* JWT Refresh Token 발급 헤더에 넣던지, 쿠키로 전달해주던지. */
+        log.info("refresh token 발급");
+        Cookie refreshTokenCookie = new Cookie(REFRESH_HEADER,"Bearer-" + jwtUtil.issueRefreshToken(authentication));
+        refreshTokenCookie.setPath("/");
+        refreshTokenCookie.setHttpOnly(true);
+        response.addCookie(refreshTokenCookie);
+
+
+        /* 유저 소셜 정보
+        * Json으로 변환하여 쿠키에 담아 프론트 엔드에 전달.
+        * utf-8 encoding 하지 않으면 한글, 공백 등 불가. */
+        ObjectMapper mapper = new ObjectMapper();
+        String jsonStr = mapper.writeValueAsString(attributes);
+
+        Cookie userInfoCookie = new Cookie("user_Info",
+                URLEncoder.encode(jsonStr,"utf-8"));
+
+        userInfoCookie.setPath("/");
+        response.addCookie(userInfoCookie);
+
+
+        /* 토큰에는 중복이 안되고, 모든 소셜에서 사용하는 컬럼인 userEmail, Provider 정보만 */
+
+
+
+
+        log.info("onAuthenticationSuccess() end");
+
+
+        /* 마지막에 프론트 엔드 로그인 완료 화면으로 리다이렉트. */
+        getRedirectStrategy().sendRedirect(request, response, targetUrl);
+
+
+
+    }
+}

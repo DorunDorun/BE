b3941e041b72e80f8bce14db98d07eb645b88555
hyunjunhwang1994 <hyunjunhwang1994@gmail.com>
2023-02-13 13:42:43 +0900

    Security Filter Config / OAuth Server, Jwt 필터 설정 추가

--- src/main/java/shop/dodotalk/dorundorun/security/config/WebSecurityConfig.java
+++ src/main/java/shop/dodotalk/dorundorun/security/config/WebSecurityConfig.java
@@ -1,31 +1,36 @@
 package shop.dodotalk.dorundorun.security.config;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import shop.dodotalk.dorundorun.security.handler.OAuth2SuccessHandler;
+import shop.dodotalk.dorundorun.security.service.OAuthService;
+import shop.dodotalk.dorundorun.security.jwt.JwtFilterConfigurer;
+import shop.dodotalk.dorundorun.security.jwt.JwtUtil;
+import shop.dodotalk.dorundorun.users.service.UserPrincipalService;
 
-import java.io.OutputStream;
-
-
+@RequiredArgsConstructor
 @EnableWebSecurity
-
 public class WebSecurityConfig {
 
+    private final OAuthService oAuthService;
+    private final OAuth2SuccessHandler successHandler;
+    private final JwtUtil jwtUtil;
+    private final UserPrincipalService userPrincipalService;
 
     @Bean
     public WebSecurityCustomizer webSecurityCustomizer() {
 
         return (web) -> web.ignoring()
+                .requestMatchers(PathRequest.toH2Console())
                 .requestMatchers(PathRequest.toStaticResources().atCommonLocations());
     }
 
@@ -33,13 +38,35 @@ public class WebSecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
-        // csrf 설정 해제.
+        /* 대략적인 로직
+        * 1. JWT 토큰 검사 -> 성공 시 로그인 성공
+        * 2. 실패 시 -> 다음 필터 진행
+        * 3. OAuth 인증 로직 진행
+        * */
+
+        /* csrf 설정가 해제. */
         http.csrf().disable();
 
 
+        /* JWT */
+        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+        http.apply(new JwtFilterConfigurer(jwtUtil, userPrincipalService));
+
+
+        /* URL Mapping */
         http.authorizeRequests()
-                // 일단 기본 설정으로 전부 허용.
-                .antMatchers("/**").permitAll();
+                .antMatchers("/oauth/**").permitAll()
+                .antMatchers("/").permitAll()
+                .anyRequest().authenticated();
+
+
+
+        /* Oauth2 */
+        http.oauth2Login()
+                .successHandler(successHandler)
+                .userInfoEndpoint()
+                .userService(oAuthService);
+
 
         // cors 설정.
         http.cors().configurationSource(corsConfigurationSource());
@@ -52,7 +79,8 @@ public class WebSecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        // 일단 기본 설정으로 전부 허용.
+        /* 일단 기본 설정으로 전부 허용.
+        * todo 서버 배포 시 프론트엔드 서버로 바꿀 예정 */
         configuration.addAllowedOriginPattern("*");
         configuration.addAllowedMethod("*");
         configuration.addAllowedHeader("*");

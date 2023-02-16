589958e78bcd68345731e4ee98d1890f1aa6d526
hyunjunhwang1994 <hyunjunhwang1994@gmail.com>
2023-02-16 13:58:19 +0900

    domain category 구현

---
+++ src/main/java/shop/dodotalk/dorundorun/chatroom/entity/Category.java
@@ -0,0 +1,33 @@
+package shop.dodotalk.dorundorun.chatroom.entity;
+
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.Getter;
+
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+public class Category {
+
+    @Id
+    private Long id;
+
+
+    @Column(nullable = false)
+    @Enumerated(value = EnumType.STRING)
+    private CategoryEnum category;
+
+    @JsonIgnore
+    @OneToMany(mappedBy = "category")
+    private List<Saying> sayings = new ArrayList<>();
+
+    @JsonIgnore
+    @OneToMany(mappedBy = "category")
+    private List<Room> rooms = new ArrayList<>();
+
+
+}
---
+++ src/main/java/shop/dodotalk/dorundorun/chatroom/entity/CategoryEnum.java
@@ -0,0 +1,6 @@
+package shop.dodotalk.dorundorun.chatroom.entity;
+
+/* 방 카테고리 */
+public enum CategoryEnum {
+    HOBBY, INVESTMENT, STUDY
+}

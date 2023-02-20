5247fcc8465512f9655db7ea6ed672b85cbfac68
kwon woo <dirn0568@naver.com>
2023-02-20 14:14:06 +0900

    swagger-update

--- src/main/java/shop/dodotalk/dorundorun/DorunDorunApplication.java
+++ src/main/java/shop/dodotalk/dorundorun/DorunDorunApplication.java
@@ -2,11 +2,10 @@ package shop.dodotalk.dorundorun;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableJpaAuditing
-@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
+@SpringBootApplication
 public class DorunDorunApplication {
 
     public static void main(String[] args) {

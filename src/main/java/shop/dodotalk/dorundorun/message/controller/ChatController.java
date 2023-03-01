7cdd0a15d378cc3d8f8058c06b1f2644a49462d2
kwon woo <dirn0568@naver.com>
2023-03-01 21:04:45 +0900

    chatController -> redis update

--- src/main/java/shop/dodotalk/dorundorun/message/controller/ChatController.java
+++ src/main/java/shop/dodotalk/dorundorun/message/controller/ChatController.java
@@ -1,6 +1,9 @@
 package shop.dodotalk.dorundorun.message.controller;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.redis.listener.ChannelTopic;
+import org.springframework.data.redis.listener.RedisMessageListenerContainer;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.simp.SimpMessageSendingOperations;
 import org.springframework.stereotype.Controller;
@@ -10,20 +13,29 @@ import shop.dodotalk.dorundorun.message.dto.ChatMessageResponseDto;
 import shop.dodotalk.dorundorun.message.service.ChatMessageService;
 
 
-@RequiredArgsConstructor
+
+@Slf4j
 @Controller
+@RequiredArgsConstructor
 public class ChatController {
     private final SimpMessageSendingOperations messagingTemplate;
     private final ChatMessageService chatMessageService;
+    private final RedisMessageListenerContainer redisMessageListenerContainer;
     @ResponseBody
     //@CrossOrigin(origins = "https://dorundorun.shop")
     @MessageMapping("/chat/room")
     public void message(ChatMessageRequestDto chatMessageRequestDto) {
-        System.out.println("방 ID : " + chatMessageRequestDto.getSessionId());
-        System.out.println("메세지 바이트 코드 : " + chatMessageRequestDto.getImgByteCode());
-        System.out.println("방 메세지 : " + chatMessageRequestDto.getMessage());
+        log.info("방 ID : " + chatMessageRequestDto.getSessionId());
+        log.info("메세지 바이트 코드 : " + chatMessageRequestDto.getImgByteCode());
+        log.info("방 메세지 : " + chatMessageRequestDto.getMessage());
+
         ChatMessageResponseDto chatMessageResponseDto = chatMessageService.ChatMessageCreate(chatMessageRequestDto);
 
-        messagingTemplate.convertAndSend("/sub/chat/room/" + chatMessageRequestDto.getSessionId(), chatMessageResponseDto);
+        ChannelTopic topic = new ChannelTopic(chatMessageResponseDto.getSessionId());
+        redisMessageListenerContainer.addMessageListener(redisSubscriber, topic);
+
+        redisPublisher.publish(topic, chatMessage);
+
+//        messagingTemplate.convertAndSend("/sub/chat/room/" + chatMessageRequestDto.getSessionId(), chatMessageResponseDto);
     }
 }

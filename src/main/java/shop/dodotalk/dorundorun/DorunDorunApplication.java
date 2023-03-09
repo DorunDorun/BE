6f7ad1de4e8f25c199173a69730a3e0c8b6edc87
hyunjunhwang1994 <hyunjunhwang1994@gmail.com>
2023-03-09 11:45:27 +0900

    실제로 방에 인원이 존재하지 않지만, 무언가 버그로 인해인원이 있는 것으로 간주하여 존재하는 허구의 방을 스케줄러로 5분에 한 번씩 삭제하는 기능 구현

--- src/main/java/shop/dodotalk/dorundorun/DorunDorunApplication.java
+++ src/main/java/shop/dodotalk/dorundorun/DorunDorunApplication.java
@@ -3,9 +3,13 @@ package shop.dodotalk.dorundorun;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableJpaAuditing
 @SpringBootApplication
+@EnableScheduling
+@EnableAsync
 public class DorunDorunApplication {
 
     public static void main(String[] args) {

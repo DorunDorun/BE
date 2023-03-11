88520926e532191d92384cdf14b96b221967bfd3
kwon woo <dirn0568@naver.com>
2023-03-11 21:05:10 +0900

    sessions 추가

--- src/main/java/shop/dodotalk/dorundorun/message/config/WebSockConfig.java
+++ src/main/java/shop/dodotalk/dorundorun/message/config/WebSockConfig.java
@@ -3,6 +3,7 @@ package shop.dodotalk.dorundorun.message.config;
 
 import org.springframework.messaging.simp.config.ChannelRegistration;
 import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
+import org.springframework.messaging.simp.stomp.StompSession;
 import org.springframework.web.socket.config.annotation.*;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import lombok.RequiredArgsConstructor;
@@ -18,6 +19,9 @@ import org.springframework.web.socket.messaging.SessionConnectEvent;
 import org.springframework.web.socket.messaging.SessionDisconnectEvent;
 import shop.dodotalk.dorundorun.message.handler.StompHandler;
 
+import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
+
 
 @Slf4j
 @Configuration
@@ -25,6 +29,7 @@ import shop.dodotalk.dorundorun.message.handler.StompHandler;
 @RequiredArgsConstructor
 public class WebSockConfig implements WebSocketMessageBrokerConfigurer {
     private final StompHandler stompHandler; // jwt 인증
+    private final List<StompSession> sessions = new CopyOnWriteArrayList<>();
     @Override
     public void configureMessageBroker(MessageBrokerRegistry config) {
         config.enableSimpleBroker("/sub");
@@ -47,11 +52,23 @@ public class WebSockConfig implements WebSocketMessageBrokerConfigurer {
 
     @EventListener
     public void connectEvent(SessionConnectEvent sessionConnectEvent){
+        log.info("1111111111111111111111111111111111111111");
         log.info("socket 연결 성공");
+        log.info(String.valueOf(sessionConnectEvent.getUser()));
+        log.info(String.valueOf(sessionConnectEvent));
+        sessions.add((StompSession) sessionConnectEvent);
+        log.info(String.valueOf(sessions.size()));
+        log.info("1111111111111111111111111111111111111111");
     }
     @EventListener
     public void onDisconnectEvent(SessionDisconnectEvent sessionDisconnectEvent) {
+        log.info("2222222222222222222222222222222222222222222222");
         log.info("socket 연결 끊어짐");
+        log.info(String.valueOf(sessionDisconnectEvent.getUser()));
+        log.info(String.valueOf(sessionDisconnectEvent));
+        sessions.remove((StompSession) sessionDisconnectEvent);
+        log.info(String.valueOf(sessions.size()));
+        log.info("2222222222222222222222222222222222222222222222");
     }
 
 //    @EventListener

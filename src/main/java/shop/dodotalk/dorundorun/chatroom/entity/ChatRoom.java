0d1d0379717096a6fce99aea407b6ac1ff5dc608
hyunjunhwang1994 <hyunjunhwang1994@gmail.com>
2023-03-12 23:32:03 +0900

    히스토리 채팅방 기능으로 인해, 구조 변경, 기존에 ChatRoomUserList를 삭제된 데이터 까지 가지고 올 필요가 있어 전체적으로 사용하던 @Where절 삭제

--- src/main/java/shop/dodotalk/dorundorun/chatroom/entity/ChatRoom.java
+++ src/main/java/shop/dodotalk/dorundorun/chatroom/entity/ChatRoom.java
@@ -4,7 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import org.hibernate.annotations.Where;
+
 import shop.dodotalk.dorundorun.users.entity.User;
 
 import javax.persistence.*;
@@ -20,8 +20,6 @@ public class ChatRoom extends Timestamped {
 
     @Id
     private String sessionId;      // 방 번호
-    // Openvidu에서 발급된 해당 채팅방에 입장하기 위한 세션 (세션 == 채팅방)
-    // 다른 유저들이 해당 채팅방에 입장 요청시 해당 컬럼을 사용하여 오픈비두에 다른 유저들의 채팅방 입장을 위한 토큰을 생성합니다.
 
     @Column(nullable = false)
     private String title;       // 방 제목
@@ -33,10 +31,8 @@ public class ChatRoom extends Timestamped {
     private boolean status;      // 방 상태(public / private)
 
 
-
     @Column
     private String password;    // private시 사용할 패스워드
-
 
 
     @ManyToOne
@@ -50,8 +46,6 @@ public class ChatRoom extends Timestamped {
     private Long masterUserId;            // 방 생성자 유저 고유번호.
 
 
-
-    @Where(clause = "is_delete = false")
     @OneToMany(mappedBy = "sessionId", fetch = FetchType.LAZY, cascade = CascadeType.ALL, orphanRemoval = true)
     private List<ChatRoomUser> chatRoomUserList;
 
@@ -64,12 +58,12 @@ public class ChatRoom extends Timestamped {
     @Column
     private LocalDateTime roomDeleteTime;
 
-    public void deleteRoom (LocalDateTime roomDeleteTime) {
+    public void deleteRoom(LocalDateTime roomDeleteTime) {
         this.isDelete = true;
         this.roomDeleteTime = roomDeleteTime;
     }
 
-    public void rename(String updateTitle){
+    public void rename(String updateTitle) {
         this.title = updateTitle;
     }
 

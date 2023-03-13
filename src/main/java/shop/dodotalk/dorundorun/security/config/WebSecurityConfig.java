db2f151efbd0e3a9c8a8bacdff867791b317a923
kwon woo <dirn0568@naver.com>
2023-03-13 16:46:01 +0900

    버셀 주소 변경

--- src/main/java/shop/dodotalk/dorundorun/security/config/WebSecurityConfig.java
+++ src/main/java/shop/dodotalk/dorundorun/security/config/WebSecurityConfig.java
@@ -85,7 +85,7 @@ public class WebSecurityConfig {
         /* Oauth2 */
         http.oauth2Login()
                 .successHandler(successHandler)
-                .failureUrl("https://dorundourn.vercel.app/Login")/*OAuth 로그인 중 실패시 프론트 URL로 리다이렉트 하기*/
+                .failureUrl("https://dorun-dorun.vercel.app/Login")/*OAuth 로그인 중 실패시 프론트 URL로 리다이렉트 하기*/
                 .userInfoEndpoint()
                 .userService(oAuthService);
 
@@ -105,7 +105,7 @@ public class WebSecurityConfig {
 
 //        configuration.addAllowedOriginPattern("*");
         configuration.addAllowedOrigin("http://localhost:3000");
-        configuration.addAllowedOrigin("https://dorundourn.vercel.app");
+        configuration.addAllowedOrigin("https://dorun-dorun.vercel.app");
         configuration.addAllowedMethod("*");
         configuration.addAllowedHeader("*");
         configuration.addExposedHeader("Set-Cookie");

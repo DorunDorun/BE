dc7e5dc8b4a17b1b38bf70069e38c8722dd56d7b
hyunjunhwang1994 <hyunjunhwang1994@gmail.com>
2023-03-15 01:06:53 +0900

    히스토리 채팅방 보기 기능 추가

--- src/main/java/shop/dodotalk/dorundorun/chatroom/repository/ChatRoomRepository.java
+++ src/main/java/shop/dodotalk/dorundorun/chatroom/repository/ChatRoomRepository.java
@@ -1,11 +1,14 @@
 package shop.dodotalk.dorundorun.chatroom.repository;
 
+import org.hibernate.annotations.Filter;
+import org.hibernate.annotations.FilterJoinTable;
 import org.hibernate.annotations.Where;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Lock;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.jpa.repository.QueryHints;
 import org.springframework.data.repository.query.Param;
 import org.springframework.security.core.parameters.P;
 import org.springframework.stereotype.Repository;
@@ -16,8 +19,13 @@ import shop.dodotalk.dorundorun.chatroom.entity.ChatRoomUser;
 
 
 import javax.persistence.LockModeType;
+import javax.persistence.NamedQuery;
+import javax.persistence.QueryHint;
+import javax.transaction.Transactional;
 import java.util.List;
 import java.util.Optional;
+
+import static org.hibernate.jpa.QueryHints.HINT_CACHEABLE;
 
 
 @Repository
@@ -25,39 +33,35 @@ public interface ChatRoomRepository extends JpaRepository<ChatRoom, String> {
     void delete(ChatRoom room);
 
 
-    /*전체 방 조회*/
-    Page<ChatRoom> findByIsDeleteAndCntUserAfterOrderByModifiedAtDesc(boolean isDelete, Long cntUser, Pageable pageable);
-
-
-    /*전체 방 조회*/
-    @Query("select distinct room from ChatRoom room " +
-            "join room.chatRoomUserList user " +
-            "where room.isDelete = :isDelete " +
-            "and user.isDelete = :isDelete " +
-            "or user = null " +
-            "order by room.modifiedAt DESC")
+    /*전체 채팅방 조회*/
+    @Query(
+            value = "SELECT distinct room FROM ChatRoom room " +
+                    "left JOIN FETCH room.chatRoomUserList user " +
+                    "WHERE room.isDelete = false " +
+                    "AND user.isDelete = :isDelete " +
+                    "AND room.cntUser > 0 " +
+                    "or room.master = '두런두런' " +
+                    "ORDER BY room.modifiedAt DESC",
+            countQuery = "SELECT count(distinct room) FROM ChatRoom room " +
+                    "left JOIN room.chatRoomUserList user " +
+                    "WHERE room.isDelete = false " +
+                    "AND user.isDelete = :isDelete " +
+                    "AND room.cntUser > 0 " +
+                    "or room.master = '두런두런'"
+    )
     Page<ChatRoom> findByIsDelete(@Param("isDelete") boolean isDelete, Pageable pageable);
 
 
-    /*사용자가 입장했던 전체 방 조회
-     * user가 null이거나 user가 자기 자신 */
-//    @Query("select c from ChatRoom c left join c.chatRoomUserList item where c.masterUserId = :masterId or item.isDelete = :isDelete or item.userId = :userId order by c.modifiedAt DESC")
-    @Query("select distinct room from ChatRoom room " +
-            "left join room.chatRoomUserList user " +
-            "where (room.isDelete = :isDelete2 " +
-            "and room.masterUserId = :userId " +
-            "or room.isDelete = :isDelete2 " +
-            "and user.userId = :userId " +
-            "and user.isDelete = :isDelete) " +
-            "and room.cntUser > :cntUser " +
-            "order by room.modifiedAt DESC")
-    Page<ChatRoom> findByUserIdAndIsDelete(
-            @Param("userId") Long userId,
-            @Param("isDelete") boolean isDelete,
-            @Param("isDelete2") boolean isDelete2,
-            @Param("cntUser") Long cntUser,
-            Pageable pageable);
-
+    /*히스토리 채팅방 전체 조회.*/
+    @Query(
+            value = "SELECT distinct room FROM ChatRoom room " +
+                    "left JOIN room.chatRoomUserList user " +
+                    "WHERE room.isDelete = false " +
+                    "AND user.userId = :userId " +
+                    "AND room.cntUser > 0 " +
+                    "ORDER BY room.modifiedAt DESC"
+    )
+    Page<ChatRoom> findByUserId(@Param("userId") Long userId, Pageable pageable);
 
 
     @Lock(LockModeType.PESSIMISTIC_WRITE)
@@ -69,41 +73,68 @@ public interface ChatRoomRepository extends JpaRepository<ChatRoom, String> {
 
     Long countAllBy();
 
-    /* 채팅방 검색 */
-    @Query("select chatRoom from ChatRoom chatRoom WHERE " +
-            "(chatRoom.title like %:title% " +
-            "or chatRoom.subtitle like %:subtitle%) " +
-            "and chatRoom.cntUser > :cntUser ")
-    Page<ChatRoom> findByCntUserAfterAndTitleContainingOrSubtitleContainingOrderByModifiedAtDesc(
-            @Param("cntUser") Long cntUser,
-            @Param("title") String title,
-            @Param("subtitle") String subtitle,
-            Pageable pageable);
 
-    //    /*히스토리 채팅방 검색*/
+    @Query(
+            value = "SELECT distinct room FROM ChatRoom room " +
+                    "LEFT JOIN FETCH room.chatRoomUserList user " +
+                    "WHERE ((room.title like %:title% " +
+                    "or room.subtitle like %:subtitle%) " +
+                    "And room.isDelete = false " +
+                    "AND user.isDelete = false " +
+                    "AND room.cntUser > 0) " +
+                    "or ((room.title like %:title% " +
+                    "or room.subtitle like %:subtitle%) " +
+                    "and room.master = '두런두런') " +
+                    "ORDER BY room.modifiedAt DESC",
+            countQuery = "SELECT count(distinct room) FROM ChatRoom room " +
+                    "left JOIN room.chatRoomUserList user " +
+                    "WHERE ((room.title like %:title% " +
+                    "or room.subtitle like %:subtitle%) " +
+                    "and room.isDelete = false " +
+                    "AND user.isDelete = false " +
+                    "AND room.cntUser > 0) " +
+                    "or ((room.title like %:title% " +
+                    "or room.subtitle like %:subtitle%) " +
+                    "and room.master = '두런두런') "
+    )/*채팅방 검색하기.*/
+    Page<ChatRoom> findByTitleContainingOrSubtitleContaining(@Param("title") String title,
+                                                             @Param("subtitle") String subtitle,
+                                                             Pageable pageable);
+
+
+    /*히스토리 채팅방 검색*/
     @Query("select distinct room from ChatRoom room " +
             "left join room.chatRoomUserList user " +
-            "where room.isDelete = :isDelete2 " +
-            "and room.masterUserId = :userId " +
-            "or room.isDelete = :isDelete2 " +
+            "where room.isDelete = false " +
             "and user.userId = :userId " +
-            "and user.isDelete = :isDelete " +
-            "group by room.title, room.subtitle " +
-            "HAVING (room.title like %:title% " +
+            "and room.cntUser > 0 " +
+            "and (room.title like %:title% " +
             "or room.subtitle like %:subtitle%) " +
-            "and room.cntUser > :cntUser " +
             "order by room.modifiedAt DESC")
-    Page<ChatRoom> findByUserIdAndIsDelete(@Param("title") String title, @Param("subtitle") String subtitle,
-                                           @Param("userId") Long userId,
-                                           @Param("isDelete") boolean isDelete,
-                                           @Param("isDelete2") boolean isDelete2,
-                                           @Param("cntUser") Long cntUser,
-                                           Pageable pageable);
+    Page<ChatRoom> findByUserId(@Param("title") String title, @Param("subtitle") String subtitle,
+                                @Param("userId") Long userId,
+                                Pageable pageable);
 
-
-    /*카테고리 클릭 시 해당 카테고리 글 반환*/
-    Page<ChatRoom> findByIsDeleteAndCategoryAndCntUserAfterOrderByModifiedAtDesc(boolean isDelete, Category category,
-                                                                                 Long cntUser, Pageable pageable);
+    @Query(
+            value = "SELECT distinct room FROM ChatRoom room " +
+                    "left JOIN FETCH room.chatRoomUserList user " +
+                    "WHERE room.isDelete = false " +
+                    "AND user.isDelete = false " +
+                    "AND room.cntUser > 0 " +
+                    "and room.category = :category " +
+                    "or room.master = '두런두런' " +
+                    "and room.category = :category " +
+                    "ORDER BY room.modifiedAt DESC",
+            countQuery = "SELECT count(distinct room) FROM ChatRoom room " +
+                    "left JOIN room.chatRoomUserList user " +
+                    "WHERE room.isDelete = false " +
+                    "AND user.isDelete = false " +
+                    "AND room.cntUser > 0 " +
+                    "and room.category = :category " +
+                    "or room.master = '두런두런'" +
+                    "and room.category = :category "
+    )/*해당 Category 전체 채팅방 조회하기.*/
+    Page<ChatRoom> findByCategory(@Param("category") Category category, Pageable pageable);
 
 
     List<ChatRoom> findAllByIsDelete(boolean isDelete);
